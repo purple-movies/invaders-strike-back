817fec06aec58189036894f8cb45b2bdb499858a
Ash Rawstron <ash.rawstron@gmail.com>
2017-11-15 00:15:49 -0800

    Initial prototype.

---
+++ Assets/BombController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    [SerializeField] Renderer spriteRenderer;
+    [SerializeField] Collider2D circleCollider;
+    [SerializeField] Rigidbody2D body;
+    [SerializeField] GameObject explosionParticleSystem;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(spriteRenderer);
+        Destroy(circleCollider);
+        Destroy(body);
+        explosionParticleSystem.SetActive(true);
+        Destroy(gameObject, 3f);
+    }
+}
---
+++ Assets/ShipController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    private const float HORIZONTAL_VELOCITY = .08f;
+
+    [SerializeField] private GameObject bombPrefab;
+    [SerializeField] private Transform bombSpawnPoint;
+
+    private float horizontalVelocity = HORIZONTAL_VELOCITY;
+    private Vector3 positionHolder;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        horizontalVelocity = -horizontalVelocity;
+    }
+
+    private void Update()
+    {
+        positionHolder = transform.position;
+        positionHolder.x += horizontalVelocity;
+        transform.position = positionHolder;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bombPrefab, bombSpawnPoint.position, Quaternion.identity);
+        }
+    }
+}
