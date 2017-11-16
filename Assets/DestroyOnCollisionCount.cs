c5943e8155d4300dc9fd651c45d735e8a252470b
Ash Rawstron <ash.rawstron@gmail.com>
2017-11-15 21:25:13 -0800

    Add smaller test buildings and destroy-on-collision-count behavior.

---
+++ Assets/DestroyOnCollisionCount.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollisionCount : MonoBehaviour
+{
+    private int hitPoints = 10;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        subtrackLife();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        subtrackLife();
+    }
+
+    private void subtrackLife()
+    {
+        hitPoints--;
+        if (hitPoints < 0) Destroy(gameObject);
+
+        Debug.Log("hit points : " + hitPoints);
+    }
+}
