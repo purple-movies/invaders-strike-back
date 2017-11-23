01ffb0e84fc547b3cd9f162d63bec555a35b0bac
Ash Rawstron <ash.rawstron@gmail.com>
2017-11-22 17:28:20 -0800

    Add exploding targets.

---
+++ Assets/LevelController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] TargetController[] targets;
+
+	void Start ()
+    {
+		foreach (var target in targets)
+        {
+            Debug.Log("stuff : " + target);
+
+            target.onDestroyed = onTargetDestroyed;
+        }
+	}
+
+    private void onTargetDestroyed(TargetController target)
+    {
+        Debug.Log("target destroyed ::: " + target);
+    }
+}
---
+++ Assets/TargetController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    [SerializeField] Renderer spriteRenderer;
+    [SerializeField] Collider2D circleCollider;
+    [SerializeField] Rigidbody2D body;
+    [SerializeField] GameObject explosionParticleSystem;
+
+    public Action<TargetController> onDestroyed;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.otherCollider.tag.Equals("Bomb"))
+
+        if (collision.collider.tag.Equals("Bomb"))
+        {
+            Debug.Log("bombs collide !!!");
+
+            Destroy(spriteRenderer);
+            Destroy(circleCollider);
+            Destroy(body);
+            explosionParticleSystem.SetActive(true);
+            Destroy(gameObject, 3f);
+
+            if (onDestroyed != null) onDestroyed(this);
+        }
+    }
+}
